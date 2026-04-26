d64c04bff054472309c9ac9214b97c1cb59242de
traciphiadmin <admin@traciphi.com>
2026-04-26 14:33:51 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,24 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
+//import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
+    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    netlify(),
-    tanstackStart(),
+    tanstackStart({
+      prerender: {
+        enabled: true,
+        crawlLinks: true,
+      },
+    }),
     viteReact(),
   ],
   base: '/traciphi/',
   build: {
-    outDir: 'dist/client'
-  }
+    outDir: 'dist/client',
+  },
 })
 
 export default config
