2cc764b7d2eebd526324e961c39dc35672ad55b2
traciphiadmin <admin@traciphi.com>
2026-04-26 17:04:49 -0400

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,6 +5,7 @@ import {
   createRootRoute,
 } from '@tanstack/react-router'
 import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
 import '../styles.css'
 
 export const Route = createRootRoute({
